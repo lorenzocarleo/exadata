d8e95d21a8f2e984559fcae92128bd9531c5d626
lorenzocarleo <35718413+lorenzocarleo@users.noreply.github.com>
2019-02-14 16:58:41 +0000

    Update oracle_query.py

--- oracle_query.py
+++ oracle_query.py
@@ -6,10 +6,10 @@
 
 DOCUMENTATION = '''
 ---
-module: oracle_query
-short_description: Query Oracle Exadata Database and return results
+module: mysql_query
+short_description: Query MySQL/MariaDB Database and return results
 description:
-   - sqlplus SELECT, INSERT, UPDATE or DELETE query
+   - Mysql SELECT, INSERT, UPDATE or DELETE query
       - returns results on SELECT query
 
 version_added: "0.1_beta"
@@ -43,13 +43,16 @@ def read_file(module,script):
   except IOError as error:
     stderr = 'Could not open the SQL file %s.' % (error)
     module.fail_json(msg=stderr, changed=false)
-  retur
+  return sqlfile
+
 # LETS STRIP THE LINES
-def clean_sqlfile(sqlfile):
+def clean_sql(sqlfile):
   sqlfile = sqlfile.strip()
   sqlfile = sqlfile.lstrip()
   sqlfile = sqlfile.lstrip()
   sqlfile = os.linesep.join([s for s in sqlfile.splitlines() if s])
+  stdout = 'CLEAN script %s :' % (sqlfile)
+  module.exit_json(msg=stdout, changed=True)
   return sqlfile
 
 
@@ -59,16 +62,17 @@ def exec_sql(module, cursor, conn, sql):
     commit = True
   else:
     commit = False
-
   try:
+    #cursor.execute("insert into ansible1.product values (370,'raffaele')")
     cursor.execute(sql)
+    conn.commit()
+    stdout = 'EXECUTING script %s :' % (sql)
+    module.exit_json(msg=stdout, changed=True)
   except cx_Oracle.DatabaseError as error_cx:
     ecx = error_cx.args
-    stderr = 'ERROR : %s on SQL %s' % (ecx.message,sql)
-    module.fail_json(msg=stderr, changed=False)
+    stdout = 'CANNOT CONNECT T THE DATABASE %s :' % (ecx)
+    module.exit_json(msg=stdout, changed=False)
     return False
-  if commit:
-    conn.commit()
   return True
 
 
@@ -81,6 +85,7 @@ def main():
       service_name=dict(required=False, aliases=['sn']),
       hostname=dict(required=False, default='localhost', aliases=['host']),
       port=dict(required=False, default=1521),
+      sql=dict(required=False),
       script=dict(required=False)
     )
   )
@@ -93,17 +98,16 @@ def main():
   script = module.params["script"]
 
   # LETS SHOW IF THE CX_ORACLE COULD BE IMPORTED PROPERLY ON LINE 35
-  if not cx_ora:
-    stderr = 'CX_ORACLE must be installed'
-    module.fail_json(msg=stderr)
+#  if not cx_ora:
+#    stderr = 'CX_ORACLE must be installed'
+#    module.fail_json(msg=stderr)
 
   # LTES CONNECT TO THE DATABASE AND OPERATE
   service_conn = '/@%s' % (service_name)
   try:
-    dns = cx_Oracle.makedns(host=hostname, port=port, service_name=service_name)
-    connect = dns
-    conn = cx_Oracle.connect(user, password, dns)
-
+    dsn = cx_Oracle.makedsn(host=hostname, port=port, service_name=service_name)
+    connect = dsn
+    conn = cx_Oracle.connect(user, password, dsn)
   except cx_Oracle.DatabaseError as error_cx:
     ecx = error_cx.args
     stderr = 'ERROR : Could Not connect to the database %s - Service Name/Connect descriptor: %s' % (ecx.message,connect)
@@ -112,22 +116,9 @@ def main():
   cursor = conn.cursor()
 
   sqlfile = read_file(module, script)
-  if len(sqlfile) > 0:
-    sqlfile = clean_sql(sqlfile)
-    if sqlfile.endswith('/') or ('CREATE OR REPLACE') in sqlfile.upper():
-      sqldelim = '/'
-    else:
-      sqldelim = ';'
-
-    sqlfile = sqlfile.strip(sqldelim)
-
-    for query in sql:
-      execute_sql(module, cursor, conn, query)
-    stdout = 'Finished running script %s \n Content : \n%s\n' % (script, sqlfile)
-    module.exit_json(msg=stdout, changed=True)
-  else:
-    stderr = 'ERROR : File %s is empty!' % (script)
-    module.fail_json(msg=stderr, changed=False)
+  bool = exec_sql(module, cursor, conn, sqlfile)
+  stdout = 'Finished running script %s \n Content %s :' % (sqlfile, bool)
+  module.exit_json(msg=stdout, changed=True)
 
 if __name__ == '__main__':
   main()
